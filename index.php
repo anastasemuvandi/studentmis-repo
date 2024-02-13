2d1b9e699f73f805f1cf21bd5bde7e3f6f491b02
Anastase <anastasemuvandi@gmail.com>
2024-02-13 09:18:49 +0200

    Bootstrap is added

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Students MIS</title>
 </head>
 <body>
-    <h2>Welcome to students MIS</h2>
+    <div class="container">
+        <h2>Welcome to students MIS</h2>
+    </div>
 </body>
 </html>
